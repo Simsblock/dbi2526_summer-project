50689382313720d18698762561938e216fac4685
Simsblock <146435639+Simsblock@users.noreply.github.com>
2026-03-21 20:41:58 +0100

    Ridicilous Fact

--- src/factgenerator.py
+++ src/factgenerator.py
@@ -117,3 +117,41 @@ def generate_enriched_fact(df_realms,df_companies,df_species):
     fact_df['V_PlayerCount'] = fact_df.apply(calculate_players, axis=1)
 
     return fact_df
+
+def generate_ridicilous_fact(df_realms,df_companies,df_species,df_tribes):
+
+    isEndgame = [True, False]
+    realms = df_realms['Dim_Realms'].unique()
+    grand_companies = df_companies['Dim_GrandCompany'].unique()
+    species_list = df_species['Dim_Species'].unique()
+    tribe_list = df_tribes['Dim_Tribe'].unique()
+
+    gc_percentiles = dict(zip(df_companies['Dim_GrandCompany'], df_companies['Percentile']))
+    species_eg_percent = dict(zip(df_species['Dim_Species'], df_species['Endgame_Pop'] / totalPop))
+    species_total_percent = dict(zip(df_species['Dim_Species'], df_species['Total_Pop'] / totalEGPop))
+    tribe_rates = dict(zip(df_tribes['Dim_Tribe'], df_tribes['V_Interactions'] / totalPop))
+
+    combinations = list(itertools.product(isEndgame, realms, grand_companies, species_list, tribe_list))
+    fact_df = pd.DataFrame(combinations, columns=['Dim_isEndgame', 'Dim_Realms', 'Dim_GrandCompany', 'Dim_Species', 'Dim_Tribe'])
+
+
+    def calculate_players(row):
+        realm_data = df_realms[df_realms['Dim_Realms'] == row['Dim_Realms']].iloc[0]
+
+        if row['Dim_isEndgame']:
+            realm_base = realm_data['Endgame_Pop']
+            species_share = species_eg_percent.get(row['Dim_Species'], 0)
+        else:
+            realm_base = realm_data['Total_Pop'] - realm_data['Endgame_Pop']
+            species_share = species_total_percent.get(row['Dim_Species'], 0)
+
+        gc_share = gc_percentiles.get(row['Dim_GrandCompany'], 0)
+        tribe_share = tribe_rates.get(row['Dim_Tribe'], 0)
+
+        player_count = realm_base * gc_share * species_share
+        tribe_completions = player_count * tribe_share
+
+        return pd.Series([round(player_count), round(tribe_completions)])
+
+    fact_df[['V_PlayerCount', 'V_TribeCompletions']] = fact_df.apply(calculate_players, axis=1)
+    return fact_df
